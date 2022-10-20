5718e85764f263e1e26652ef3ba766e39e2f5bf7
xiejianhao <xie.jianhao@sinovatio.com>
2022-10-20 20:30:15 +0800

    首次提交

---
+++ src/main/java/com/mendas/notebook/NotebookApplication.java
@@ -0,0 +1,16 @@
+package com.mendas.notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class NotebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookApplication.class, args);
+    }
+
+}
