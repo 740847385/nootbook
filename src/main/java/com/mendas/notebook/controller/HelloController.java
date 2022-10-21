0535c6afcd0a53242bb46909dd9449611b7aef0e
xiejianhao <xie.jianhao@sinovatio.com>
2022-10-21 13:49:43 +0800

    幂等性提交

--- src/main/java/com/mendas/notebook/controller/HelloController.java
+++ src/main/java/com/mendas/notebook/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.mendas.notebook.controller;
 
+import com.mendas.notebook.utils.IdempotentUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
+
         return "hello world";
+    }
+
+
+    @GetMapping("/hello1")
+    public String hello(String id) {
+        if (!IdempotentUtils.judge(id, this.getClass())) {
+            return "执行失败";
+        }
+        System.out.println("添加用户ID:" + id);
+        return "hello world " + id;
+
     }
 }
---
+++ src/main/java/com/mendas/notebook/utils/IdempotentUtils.java
@@ -0,0 +1,29 @@
+package com.mendas.notebook.utils;
+
+import org.apache.commons.collections4.map.LRUMap;
+
+/**
+ * 幂等性判断
+ *
+ * @author Administrator
+ */
+public class IdempotentUtils {
+    /**
+     * 根据 LRU(Least Recently Used，最近最少使用)算法淘汰数据的 Map 集合，最大容量 100 个
+     */
+    private static  LRUMap<String, Integer> reqCache = new LRUMap<>(100);
+
+
+    public static boolean judge(String id, Object lockClass) {
+        synchronized (lockClass) {
+            if (reqCache.containsKey(id)) {
+                // 重复请求
+                System.out.println("请勿重复提交！！！" + id);
+                return false;
+            }
+
+            reqCache.put(id, 1);
+        }
+        return true;
+    }
+}
